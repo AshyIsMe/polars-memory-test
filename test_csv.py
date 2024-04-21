3e024efb4b80526a2da3c2583a2908c868b7895f
Aaron Ash <aaron.ash@gmail.com>
2024-04-21 19:59:52 +1000

    attempt streaming

--- test_csv.py
+++ test_csv.py
@@ -6,17 +6,13 @@ import sys
 print(f"{sys.version = }")
 print(f"{pl.__version__ = }")
 
-if not os.path.exists("df.parquet"):
+if not os.path.exists("df.csv"):
     df = pl.DataFrame({"a": np.arange(1e8), "b": np.arange(1e8), "c": np.arange(1e8)})
     print(f"{df.estimated_size()/1e9 = } GB")
-    df.write_parquet("df.parquet")
+    df.write_csv("df.csv")
     del df
 
 with pl.Config() as cfg:
     cfg.set_streaming_chunk_size(1000)
 
-    print(
-        pl.scan_parquet("df.parquet", low_memory=True, parallel="none")
-        .max()
-        .collect(streaming=True)
-    )
+    print(pl.scan_csv("df.csv", low_memory=True).max().collect(streaming=True))
